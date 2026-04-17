6fcec9039a9c1d8d9e4740cd772e34c35176201e
Dorka845 <foldvari.dorka_2022b@bolyai-szakkozep.hu>
2026-04-17 08:40:19 +0200

    feladatok folytatasa

---
+++ 07a_navbar_param/manager.js
@@ -0,0 +1,82 @@
+/**
+ * @callback RenderCallback
+ * @param {Color[]}
+ * @returns {void}
+ * 
+ * @typedef ColorType
+ * @param {string} szin
+ */
+
+class Manager {
+    /**@type {Color[]} */
+    #list;
+    /**@type {RenderCallback} */
+    #renderCallback;
+
+    /**
+     * @param {ColorType} data 
+     */
+    constructor(data) {
+        this.#list = [];
+    }
+
+    set renderCallback(callback) {
+        this.#renderCallback = callback;
+    }
+    /**
+     * @returns {void}
+     */
+    getAllElement() {
+        if (this.#renderCallback) {
+            this.#renderCallback(this.#list);
+        }
+    }
+
+    /**
+     * @param {ColorType} elem 
+     */
+    addElement(elem) {
+        const newColor = new Color();
+        newColor.color = elem.szin;
+        this.#list.push(newColor);
+        this.getAllElement();
+    }
+
+    /**
+     * @param {number} id 
+     * @param {ColorType} elem 
+     */
+    updateElement(id, elem) {
+        const target = this.#list.find(c => c.id === id);
+        if (target) {
+            target.color = elem.szin;
+            this.getAllElement();
+        }
+    }
+}
+
+class Color {
+    #id;
+    #color;
+
+    constructor() {
+
+    }
+
+    get id() {
+        return this.#id;
+    }
+    /**@type {number} */
+    set id(value) {
+        this.#id = value;
+    }
+    get color() {
+        return this.#color;
+    }
+    /**@type {string} */
+    set color(value) {
+        this.#color = value;
+    }
+}
+
+export {Manager}
